b258deb13d48a4ed045731d3bb624e0a1fe49870
NathanJWr <nathanwright1623@gmail.com>
2018-06-26 17:29:15 -0700

    changes

--- player.c
+++ player.c
@@ -2,15 +2,21 @@
 extern const int SCREEN_W;
 extern const int SCREEN_H;
 void playerMoveUp(struct Player* p) {
-        p -> y -= p -> dy;
+        if(p -> y  > 0){
+                p -> y -= p -> dy;
+        }
 }
 
 void playerMoveDown(struct Player* p) {
-        p -> y += p -> dy;
+        if(p -> y + p -> sizeY < SCREEN_H) {
+                p -> y += p -> dy;
+        }
 }
 
 void playerMoveLeft(struct Player* p) {
-        p -> x -= p -> dx;
+        if(p -> x > 0) {
+                p -> x -= p -> dx;
+        }
 }
 
 void playerMoveRight(struct Player* p) {
