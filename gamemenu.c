1178131c9b225bbb25ff1055e4f5f95a81d67fc0
NathanJWr <nathanwright1623@gmail.com>
2018-08-27 18:21:27 -0700

    Made the game menu code a bit cleaner and in the same style as the main menu code

--- gamemenu.c
+++ gamemenu.c
@@ -1,4 +1,5 @@
 #include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_image.h>
 #include <SDL2/SDL.h>
 #include "gamemenu.h"
 #include "display.h"
@@ -7,42 +8,76 @@ extern SDL_Renderer* renderer;
 extern const int SCREEN_W;
 extern const int SCREEN_H;
 
-SDL_Texture* menu_texture;
-SDL_Texture* continue_texture;
-SDL_Rect menu_pos;
-SDL_Rect continue_pos;
+struct GameMenuAssets game_menu;
 
-int menu_w;
-int menu_h;
-int continue_w;
-int continue_h;
+/*
+Return 0 for main_menu
+Return 1 for Continue
+Return -1 for nothing
+*/
 int mousePressGame(SDL_MouseButtonEvent b) {
         if(b.button == SDL_BUTTON_LEFT) {
                 //Continue Button
-                if(b.x >= continue_pos.x
-                                && b.x <= continue_pos.x + continue_w
-                                && b.y >= continue_pos.y
-                                && b.y <= continue_pos.y + continue_h) {
+                if(b.x >= game_menu.continue_pos.x
+                                && b.x <= game_menu.continue_pos.x 
+                                        + game_menu.continue_pos.w
+                                && b.y >= game_menu.continue_pos.y
+                                && b.y <= game_menu.continue_pos.y 
+                                        + game_menu.continue_pos.h) {
                         return 1;
                 }
+
                 //Main Menu Button
-                else if(b.x >= menu_pos.x
-                                && b.x <= menu_pos.x + menu_w
-                                && b.y >= menu_pos.y
-                                && b.y <= menu_pos.y + menu_h) {
+                if(b.x >= game_menu.menu_pos.x
+                                && b.x <= game_menu.menu_pos.x 
+                                        + game_menu.menu_pos.w
+                                && b.y >= game_menu.menu_pos.y
+                                && b.y <= game_menu.menu_pos.y 
+                                        + game_menu.menu_pos.h) {
                         return 0;
                 }
                 else return -1;
         }
         else return -1;
 }
+
+void mouseSelectGame() {
+        int mouse_x, mouse_y;
+        SDL_GetMouseState(&mouse_x, &mouse_y);
+        if(mouse_x >= game_menu.continue_pos.x
+                        && mouse_x <= game_menu.continue_pos.x 
+                                + game_menu.continue_pos.w
+                        && mouse_y >= game_menu.continue_pos.y
+                        && mouse_y <= game_menu.continue_pos.y 
+                                + game_menu.continue_pos.h) {
+
+                game_menu.is_continue_selected = true;
+        }
+        else {
+                game_menu.is_continue_selected = false;
+        }
+        if(mouse_x >= game_menu.menu_pos.x
+                        && mouse_x <= game_menu.menu_pos.x 
+                                + game_menu.menu_pos.w
+                        && mouse_y >= game_menu.menu_pos.y
+                        && mouse_y <= game_menu.menu_pos.y 
+                                + game_menu.menu_pos.h) {
+
+                game_menu.is_menu_selected = true;
+        }
+        else {
+                game_menu.is_menu_selected = false;
+        }
+}
 /*
 Return 0 for main_menu
 Return 1 for Continue
+Return -1 for nothing
 */
 int handleGameMenuEvents(SDL_Event* e) {
         int button = -1;
         while(SDL_PollEvent(e)) {
+                mouseSelectGame();
                 if(e -> type == SDL_QUIT) {
                        button = 0;
                 }
@@ -54,22 +89,23 @@ int handleGameMenuEvents(SDL_Event* e) {
         return -1;
 }
 void setupGameMenu() {
-        SDL_Color text_color = {255, 255, 255, 255};
-        SDL_Surface* main_menu = TTF_RenderText_Solid(font, "Main Menu", text_color);
-        SDL_Surface* cont = TTF_RenderText_Solid(font, "Continue", text_color);
+        game_menu.menu_white = IMG_LoadTexture(renderer,
+                        "assets/mainmenu_text.png");
+        game_menu.menu_green = IMG_LoadTexture(renderer,
+                        "assets/mainmenu_text_green.png");
+        game_menu.menu_pos.w = 800;
+        game_menu.menu_pos.h = 100;
+        game_menu.menu_pos.x = (SCREEN_W / 2) - game_menu.menu_pos.w / 2;
+        game_menu.menu_pos.y = (SCREEN_H / 2) + 20;
 
-        menu_w = main_menu -> w;
-        menu_h = main_menu -> h;
-        continue_w = cont -> w;
-        continue_h = cont -> h;
-
-        continue_pos.x = (SCREEN_W / 2) - continue_w / 2;
-        continue_pos.y = (SCREEN_H / 2) - 100;
-        menu_pos.x = (SCREEN_W / 2) - menu_w / 2;
-        menu_pos.y = (SCREEN_H / 2) + 10;
-
-        menu_texture = surfaceToTexture(main_menu);
-        continue_texture = surfaceToTexture(cont);
+        game_menu.continue_white = IMG_LoadTexture(renderer, 
+                        "assets/continue_text.png");
+        game_menu.continue_green = IMG_LoadTexture(renderer,
+                        "assets/continue_text_green.png");
+        game_menu.continue_pos.w = 800;
+        game_menu.continue_pos.h = 100;
+        game_menu.continue_pos.x = (SCREEN_W / 2) - game_menu.continue_pos.w / 2;
+        game_menu.continue_pos.y = (SCREEN_H / 2) - 100;
 }
 
 void updateGameMenu() {
@@ -80,18 +116,33 @@ void updateGameMenu() {
 
 void drawGameMenu(int game_outcome) {
         if(game_outcome == 0) {
-                SDL_QueryTexture(continue_texture, NULL, NULL, &continue_pos.w, &continue_pos.h);
-                SDL_QueryTexture(menu_texture, NULL, NULL, &menu_pos.w, &menu_pos.h);
-                SDL_RenderCopy(renderer, menu_texture, NULL, &menu_pos);
-                SDL_RenderCopy(renderer, continue_texture, NULL, &continue_pos);
+                if(game_menu.is_menu_selected) {
+                        SDL_RenderCopy(renderer, game_menu.menu_green,
+                                        NULL, &game_menu.menu_pos);
+                }
+                else {
+                        SDL_RenderCopy(renderer, game_menu.menu_white,
+                                        NULL, &game_menu.menu_pos);
+                }
+
+                if(game_menu.is_continue_selected) {
+                        SDL_RenderCopy(renderer, game_menu.continue_green,
+                                        NULL, &game_menu.continue_pos);
+                }
+                else {
+                        SDL_RenderCopy(renderer, game_menu.continue_white,
+                                        NULL, &game_menu.continue_pos);
+                }
         }
         if(game_outcome == 1) {
-                SDL_QueryTexture(menu_texture, NULL, NULL, &menu_pos.w, &menu_pos.h);
-                SDL_RenderCopy(renderer, menu_texture, NULL, &menu_pos);
+                SDL_RenderCopy(renderer, game_menu.menu_white, NULL,
+                                &game_menu.menu_pos);
         }
 }
 
 void cleanupGameMenu() {
-        SDL_DestroyTexture(menu_texture);
-        SDL_DestroyTexture(continue_texture);
+        SDL_DestroyTexture(game_menu.continue_white);
+        SDL_DestroyTexture(game_menu.continue_green);
+        SDL_DestroyTexture(game_menu.menu_white);
+        SDL_DestroyTexture(game_menu.menu_green);
 }
--- gamemenu.h
+++ gamemenu.h
@@ -1,6 +1,18 @@
 #ifndef GAMEMENU_H
 #define GAMEMENU_H
 #include <SDL2/SDL.h>
+#include <stdbool.h>
+struct GameMenuAssets {
+        SDL_Texture* continue_white;
+        SDL_Texture* continue_green;
+        SDL_Rect continue_pos;
+        bool is_continue_selected;
+
+        SDL_Texture* menu_white;
+        SDL_Texture* menu_green;
+        SDL_Rect menu_pos;
+        bool is_menu_selected;
+};
 int handleGameMenuEvents(SDL_Event*);
 void setupGameMenu();
 void drawGameMenu(int);
